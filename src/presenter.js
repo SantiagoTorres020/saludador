7a18366f6723ec0860b68dec429ea8370d03c5a9
Santiago Torres <santiago.torres@ucb.edu.bo>
2026-02-18 22:41:14 -0400

    feat: saluda segun la hora

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,16 @@
 const saludoP = document.querySelector("#saludo");
 
 const nombre = prompt("¿Cómo te llamas?");
-saludoP.innerHTML = "Hola, " + nombre;
+
+const hora = new Date().getHours();
+let saludo = "";
+
+if (hora >= 5 && hora <= 11) {
+  saludo = "Buenos días";
+} else if (hora >= 12 && hora <= 18) {
+  saludo = "Buenas tardes";
+} else {
+  saludo = "Buenas noches";
+}
+
+saludoP.innerHTML = saludo + ", " + nombre;
